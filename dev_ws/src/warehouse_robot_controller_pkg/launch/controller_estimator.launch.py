d82856890dbf810e081f1e0e922c203c5cd4bc74
MarcosEALima <95235470+MarcosEALima@users.noreply.github.com>
2023-10-13 00:06:48 -0300

    Update controller_estimator.launch.py

--- dev_ws/src/warehouse_robot_controller_pkg/launch/controller_estimator.launch.py
+++ dev_ws/src/warehouse_robot_controller_pkg/launch/controller_estimator.launch.py
@@ -3,16 +3,10 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    # Esta função retorna uma descrição de lançamento contendo os nós a serem executados.
-
     return LaunchDescription([
-        # Cria um nó do pacote 'warehouse_robot_controller_pkg' com o executável 'robot_controller'
-        # e exibe sua saída na tela.
         Node(package='warehouse_robot_controller_pkg', executable='robot_controller',
             output='screen'),
 
-        # Cria um nó do pacote 'warehouse_robot_controller_pkg' com o executável 'robot_estimator'
-        # e exibe sua saída na tela.
         Node(package='warehouse_robot_controller_pkg', executable='robot_estimator',
             output='screen'),
     ])
