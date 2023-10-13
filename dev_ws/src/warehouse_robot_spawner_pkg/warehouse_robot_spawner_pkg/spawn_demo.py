ca6150e7957b507a47ee8541027cb6543f12ad8a
MarcosEALima <95235470+MarcosEALima@users.noreply.github.com>
2023-10-13 00:05:44 -0300

    Update spawn_demo.py

--- dev_ws/src/warehouse_robot_spawner_pkg/warehouse_robot_spawner_pkg/spawn_demo.py
+++ dev_ws/src/warehouse_robot_spawner_pkg/warehouse_robot_spawner_pkg/spawn_demo.py
@@ -1,57 +1,36 @@
-"""
-Nó ROS 2 para gerar um robô móvel dentro de um armazém.
-
-Autor:
-  - Addison Sears-Collins
-  - https://automaticaddison.com
-"""
-import os # Biblioteca do sistema operacional
-import sys # Biblioteca do ambiente de execução do Python
-import rclpy # Biblioteca de Cliente ROS para Python
-
-# Biblioteca de Gerenciamento de Pacotes
+import os
+import sys 
+import rclpy
+#Biblioteca de Gerenciamento de Pacotes
 from ament_index_python.packages import get_package_share_directory 
-
-# Serviço do Gazebo para gerar um robô
+#Serviço do Gazebo para gerar um robô
 from gazebo_msgs.srv import SpawnEntity
 
 def main():
-
-    """ Principal para gerar um nó de robô """
-    # Obter argumentos de entrada do usuário
     argv = sys.argv[1:]
-
-    # Iniciar nó
     rclpy.init()
-
-    # Obter o caminho do arquivo para o modelo do robô
+  
     sdf_file_path = os.path.join(
         get_package_share_directory("warehouse_robot_spawner_pkg"), "models",
         "mobile_warehouse_robot", "model.sdf")
-        
-    # Criar o nó
+ 
     node = rclpy.create_node("entity_spawner")
 
-    # Mostrar progresso na janela do terminal
     node.get_logger().info(
         'Criando cliente de serviço para conectar a `/spawn_entity`')
     client = node.create_client(SpawnEntity, "/spawn_entity")
 
-    # Obter o serviço spawn_entity
     node.get_logger().info("Conectando ao serviço `/spawn_entity`...")
     if not client.service_is_ready():
         client.wait_for_service()
         node.get_logger().info("...conectado!")
 
-    # Obter caminho para o robô
     sdf_file_path = os.path.join(
         get_package_share_directory("warehouse_robot_spawner_pkg"), "models",
         "mobile_warehouse_robot", "model.sdf")
 
-    # Mostrar caminho do arquivo
-    print(f"robot_sdf={sdf_file_path}")
-    
-    # Definir dados para solicitação
+    #print(f"robot_sdf={sdf_file_path}")
+  
     request = SpawnEntity.Request()
     request.name = argv[0]
     request.xml = open(sdf_file_path, 'r').read()
